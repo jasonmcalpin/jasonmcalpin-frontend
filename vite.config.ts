eb90ba88bab1258ada7c76d3baedc3d9f36535ae
Jason McAlpin <golmsapce@gmail.com>
2025-08-06 14:25:42 -0400

    fix(optimize): added compression back since the rest of the code if properly updated. next step is adding the data files back to this.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer';
+import { compression } from 'vite-plugin-compression2';
+
+
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    compression({
+      algorithms: ['brotliCompress'],
+      threshold: 10240, 
+    }),
+    visualizer({
+      open: false,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'dist/stats.html',
+    }),
   ],
-})
+  base: '/',
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: false, 
+        drop_debugger: true,
+      },
+      mangle: true,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-redux': ['@reduxjs/toolkit', 'react-redux'],
+          'vendor-ui': ['framer-motion', 'react-intersection-observer'],
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+      },
+    },
+    chunkSizeWarningLimit: 500,
+    sourcemap: true,
+  },
+});
